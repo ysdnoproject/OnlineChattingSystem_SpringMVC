fd7517035ed9dc16a4e026655979cf01219fe31d
ysd <yuanshidong19921230@gmail.com>
2016-07-21 12:14:01 +0800

    引入log4j和slf4j

--- src/main/java/com/yuanshidong/chat/service/impl/UserServiceImpl.java
+++ src/main/java/com/yuanshidong/chat/service/impl/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package com.yuanshidong.chat.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.yuanshidong.chat.dao.IUserDao;
 import com.yuanshidong.chat.entity.User;
 import com.yuanshidong.chat.service.IUserService;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -15,8 +17,10 @@ import javax.annotation.Resource;
 public class UserServiceImpl implements IUserService {
     @Resource
     private IUserDao userDao;
+    private Logger logger = Logger.getLogger(UserServiceImpl.class);
 
     public User getUserById(int userId) {
+        logger.info(JSON.toJSONString(userId));
         return this.userDao.findbyId(userId);
     }
 }
